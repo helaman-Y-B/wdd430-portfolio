aaf21a2a01a1c91c9a0ab25387102c72c7b52388
Helamã Y. Barbour <90625373+helaman-Y-B@users.noreply.github.com>
2026-09-05 15:06:58 +0000

    Created the about page. Created the why componant and added images.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,29 @@
+import TheWhy from "@/components/about/TheWhy";
+import Image from "next/image";
+import aboutImage600 from "../../public/about/aboutCode600.webp";
+import aboutImage800 from "../../public/about/aboutCode800.webp";
+
 export default function About() {
   return (
     <main className="max-w-4xl mx-auto px-4 py-12">
       <h2 className="text-3xl font-bold mb-4">About Me</h2>
       <p className="text-lg text-gray-700">
-        This about page shares more information about my background and work.
+        I am a passionate developer with experience in creating dynamic and
+        user-friendly web applications.
+        <br />I am constantly learning new technologies and improving my skills
+        to deliver high-quality solutions. My goal is to build applications that
+        not only meet user needs but also provide an exceptional user
+        experience.
       </p>
+      <picture>
+        <source media="(min-width: 601px)" srcSet={aboutImage800.src} />
+        <Image
+          src={aboutImage600}
+          alt="About me"
+          className="mx-auto rounded-lg mt-8 mb-8"
+        />
+      </picture>
+      <TheWhy />
     </main>
   );
 }
---
+++ components/about/TheWhy.tsx
@@ -0,0 +1,19 @@
+export default function TheWhy() {
+  return (
+    <section className="max-w-4xl mx-auto p-4">
+      <h2 className="text-3xl font-bold mb-4">The Why</h2>
+      <p className="text-lg text-gray-700">
+        You might be asking "why you decided to become a web developer?", and
+        the answer is simple: Technology is a powerful tool that can be used to
+        create almost everything.
+        <br />
+        <br />
+        Because of that, I felt that learning how to create web applications
+        would be a great way to contribute to the world and make a positive
+        impact on people's lives.<br />I am passionate about using technology
+        to solve problems and create innovative solutions that can improve the
+        way we live and work.
+      </p>
+    </section>
+  );
+}
